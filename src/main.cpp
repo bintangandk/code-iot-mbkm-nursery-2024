878f8aeb7a46ecfbb3ba84308846e74496ae99c6
bintangandk <124556644+bintangandk@users.noreply.github.com>
2024-11-17 00:01:51 +0700

    add led indikator wifi connection and led send data

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-#include <WiFiClientSecure.h>  // Pustaka untuk koneksi SSL
+#include <WiFiClientSecure.h>
 #include "DHT.h"
 
 // Definisikan pin dan jenis sensor DHT
@@ -9,22 +9,32 @@
 DHT dht(DHTPIN, DHTTYPE);  // Membuat objek DHT
 
 // Informasi jaringan WiFi
-const char* ssid = "Redmi 13"; // Ganti dengan SSID WiFi Anda
-const char* password = "123456789"; // Ganti dengan password WiFi Anda
+const char* ssid = "SSID13"; // Ganti dengan SSID WiFi Anda
+const char* password = "1sampai8"; // Ganti dengan password WiFi Anda
 const char* serverUrl = "https://nursery-mbkm.research-ai.my.id/api/senddata"; // URL server
 
 WiFiClientSecure wifiClientSecure; // Membuat objek WiFiClientSecure untuk koneksi HTTPS
 
+// Pin untuk LED
+#define LED_WIFI D1 // LED WiFi
+#define LED_SEND D2 // LED indikator pengiriman data
+
 void setup() {
   Serial.begin(9600); // Memulai serial komunikasi
+  pinMode(LED_WIFI, OUTPUT); // Atur LED WiFi sebagai output
+  pinMode(LED_SEND, OUTPUT); // Atur LED pengiriman data sebagai output
+
   WiFi.begin(ssid, password); // Menghubungkan ke WiFi
 
   // Menunggu hingga terhubung ke WiFi
   Serial.print("Connecting to WiFi...");
   while (WiFi.status() != WL_CONNECTED) {
+    digitalWrite(LED_WIFI, !digitalRead(LED_WIFI)); // Berkedip
     delay(500);
     Serial.print(".");
   }
+
+  digitalWrite(LED_WIFI, HIGH); // Nyalakan LED jika terhubung
   Serial.println("Connected to WiFi");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP()); // Menampilkan IP Address ESP8266
@@ -94,12 +104,25 @@ void sendData(float temperature, float humidity) {
     if (httpResponseCode > 0) {
       String response = http.getString(); // Menerima respons dari server
       Serial.println("Response: " + response);
+
+      // LED berkedip dua kali jika berhasil
+      for (int i = 0; i < 2; i++) {
+        digitalWrite(LED_SEND, HIGH);
+        delay(200);
+        digitalWrite(LED_SEND, LOW);
+        delay(200);
+      }
     } else {
       Serial.print("Error occurred while sending data. Code: ");
       Serial.println(httpResponseCode);
+
+      // Pastikan LED indikator tetap mati jika gagal
+      digitalWrite(LED_SEND, LOW);
     }
 
     http.end(); // Mengakhiri koneksi HTTP
   } else {
     Serial.println("WiFi not connected"); // Menampilkan pesan jika tidak terhubung ke WiFi
+    digitalWrite(LED_SEND, LOW); // Pastikan LED indikator tetap mati
   }
+}
